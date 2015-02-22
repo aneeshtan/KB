855673fd7dca670ec9f4f59699e65223d8e9346c
Farshad Ghazanfari <aneeshtan@gmail.com>
2015-02-22 14:08:05 +0330

    moved

--- js/employeelist.js
+++ js/employeelist.js
@@ -13,10 +13,10 @@ function getEmployeeList() {
 		employees = data.items;
 		$.each(employees, function(index, employee) {
 			$('#employeeList').append('<li><a href="details.html?sid=' + employee.sid + '">' +
-					'<img src="images/logo.png"/>' +
-					'<h4>' + employee.title + ' </h4>' +
-					'<p>' + employee.hometext + '</p>' +
-					'<span class="ui-li-count">' + employee.counter + '</span></a></li>');
+					'<img style="float:right;" src="images/logo.png"/>' +
+					'<h4 style="direction:rtl;text-align:right;font:14px tahoma;font-weight:700;">' + employee.title + ' </h4>' +
+					'<p style="direction:rtl;text-align:right;font:11px tahoma;f">' + employee.hometext + '</p>' +
+					'<span class="ui-li-count"> بازدید  ' + employee.counter + ' </span></a></li>');
 		});
 		$('#employeeList').listview('refresh');
 	});
